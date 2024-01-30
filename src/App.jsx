5f4496305ea06ce621d32fcd34bb6b8460ab45bb
yas3jb <yasmu213@gmail.com>
2024-01-30 16:42:56 -0500

    Modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import ContactList from "./components/ContactList";
+import { useState } from "react";
 
 function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
   return (
     <>
-      <ContactList />
+      {selectedContactId ? (
+        <div>Selected Contact View</div>
+      ) : (
+        <ContactList setSelectedContactId={setSelectedContactId} />
+      )}
     </>
   );
 }
